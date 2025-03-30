415658aa5365e60d9a2a9e4e1cc3167ae2c85bce
DiegoBarrosA <diegobarrosaraya@outlook.com>
2025-03-29 22:47:53 -0300

    Added spring initializer

---
+++ src/main/java/one/expressdev/geekmer_hub/DemoApplication.java
@@ -0,0 +1,13 @@
+package one.expressdev.geekmer_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
